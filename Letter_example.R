b559e3f827e0c4cc15d505d2aabcb7d5445accc6
Chen Yang <cafferychen7850@gmail.com>
2024-09-23 11:25:49 -0500

    Complete Letter_example.R implementation for LRMultiClass
    - Finish implementation of Letter_example.R for LRMultiClass function
    - Add intercept columns to X and Xt matrices
    - Implement LRMultiClass with lambda=1 and 50 iterations
    - Enhance plotting with additional labels and titles
    - Add performance benchmarking using microbenchmark (5 repetitions)
    - Report median execution time
    - Complete all [ToDo] items in the original script
    - Demonstrate proper usage of LRMultiClass on letter dataset
    - Provide a foundation for further experimentation with different parameters

--- Letter_example.R
+++ Letter_example.R
@@ -6,29 +6,29 @@
 letter_train <- read.table("Data/letter-train.txt", header = F, colClasses = "numeric")
 Y <- letter_train[, 1]
 X <- as.matrix(letter_train[, -1])
+X <- cbind(1, X)  # Add intercept column
 
 # Testing data
 letter_test <- read.table("Data/letter-test.txt", header = F, colClasses = "numeric")
 Yt <- letter_test[, 1]
 Xt <- as.matrix(letter_test[, -1])
-
-# [ToDo] Make sure to add column for an intercept to X and Xt
+Xt <- cbind(1, Xt)  # Add intercept column
 
 # Source the LR function
 source("FunctionsLR.R")
 
-# [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out <- LRMultiClass(...)
+# Run the LRMultiClass algorithm with lambda = 1 and 50 iterations
+out <- LRMultiClass(X, Y, Xt, Yt, lambda = 1, numIter = 50)
 
+# Plot the objective function, training error, and testing error over iterations
+plot(out$objective, type = 'o', main = "Objective Function", xlab = "Iteration", ylab = "Objective Value")
+plot(out$error_train, type = 'o', main = "Training Error", xlab = "Iteration", ylab = "Error %")
+plot(out$error_test, type = 'o', main = "Testing Error", xlab = "Iteration", ylab = "Error %")
 
-# The code below will draw pictures of objective function, as well as train/test error over the iterations
-plot(out$objective, type = 'o')
-plot(out$error_train, type = 'o')
-plot(out$error_test, type = 'o')
+# Use microbenchmark to time the code with lambda=1 and 50 iterations, repeated 5 times
+library(microbenchmark)
+timing <- microbenchmark(out <- LRMultiClass(X, Y, Xt, Yt, lambda = 1, numIter = 50), times = 5)
 
-# Feel free to modify the code above for different lambda/eta/numIter values to see how it affects the convergence as well as train/test errors
-
-# [ToDo] Use microbenchmark to time your code with lambda=1 and 50 iterations. To save time, only apply microbenchmark 5 times.
-
-# [ToDo] Report the median time of your code from microbenchmark above in the comments below
-
-# Median time:  (in sec)
+# Report the median time of the code
+median_time <- median(timing$time) / 1e9  # Convert to seconds
+cat("Median time: ", median_time, " sec\n")
